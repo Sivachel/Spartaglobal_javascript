d8037b53ceb30c0d4aa6d628ccb769a5302fdf46
Sivachel <lsivachelvan@spartaglobal.com>
2018-08-07 22:46:41 +0100

    basic calculator and advanced caalculator working

--- calculator/javascipt.js
+++ calculator/javascipt.js
@@ -1,22 +1,55 @@
-var number_1 = prompt("Please enter first number");
-var op = prompt("Please enter operator");
-var number_2 = prompt("Please enter second number");
-console.log(number_1);
-console.log(op);
-console.log(number_2);
+//using the switch method to enable user to select the calculator type
+var calculator_type = prompt("Enter 1 for basic calculator, 2 for advanced calculator, 3 for BMI calculator, 4 for trip calculator");
 
-a = parseInt(number_1) ;
-b = parseInt(number_2);
-if (op == "+") { number_1 + number_2
-  console.log(a + b);
+switch (calculator_type) {
+  case "1":Basic_calculator();
+    break;
+    case "2":Advanced_calculator();
+      break;
+      case "3":BMI_calculator();
+        break;
+        case "4":Trip_calculator();
+          break;
+  default:
+}
+
+function Basic_calculator(){
+  var number_1 = parseFloat(prompt("Please enter first number"));
+  var op = prompt("Please enter operator");
+  var number_2 = parseFloat(prompt("Please enter second number"));
+
+  if (op == "+")
+  {
+    alert(number_1 + number_2);
   }
 
-if (op == "-") { number_1 - number_2
-  console.log(a - b);
+  if (op == "-")
+  {
+    alert(number_1 - number_2);
+  }
+
+  if (op == "*")
+  {
+    alert(number_1 * number_2);
+  }
+
+  if (op == "/")
+  {
+    alert(number_1 / number_2);
+  }
 }
-if (op == "*") { number_1 * number_2
-  console.log(a * b);
-}
-if (op == "/") { number_1 / number_2
-  console.log(a / b);
+
+function Advanced_calculator(){
+  var op = prompt("Enter 1 for power or 2 for squareroot");
+
+  if (op == 1){
+    var number_1 = parseFloat(prompt("Enter base number"));
+    var number_2 = parseFloat(prompt("Enter power number"));
+    alert(Math.pow(number_1,number_2));
+  }
+
+  if (op == 2){
+    var number_1 = parseFloat(prompt("Enter a number"));
+    alert(Math.sqrt(number_1));
+  }
 }
